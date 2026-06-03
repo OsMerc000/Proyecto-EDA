cc476d8ffc3722053347d51fbf5a2cdb1338a4fe
Johanes Osmayo <joh.osm.g@gmail.com>
2026-06-02 21:59:38 -0500

    Agregado un método recursivo para el push

--- src/KDTree.java
+++ src/KDTree.java
@@ -13,49 +13,34 @@ public class KDTree {
     }
 
     public void push(Punto punto) {
-        Nodo aux_ptr = this.root;
         Nodo new_nodo = new Nodo(punto);
-        if (aux_ptr == null) {
+        if (root == null) {
             root = new_nodo;
         }  else {
-            boolean inX = true;
-            while (aux_ptr != null) {
-                if (inX) {
-                    if (new_nodo.getPunto().getX() < aux_ptr.getPunto().getX()) {
-                        if (aux_ptr.getLeft() != null) {
-                            aux_ptr = aux_ptr.getLeft();
-                        } else {
-                            aux_ptr.setLeft(new_nodo);
-                            return;
-                        }
-                    } else {
-                        if (aux_ptr.getRight() != null) {
-                            aux_ptr = aux_ptr.getRight();
-                        } else {
-                            aux_ptr.setRight(new_nodo);
-                            return;
-                        }
-                    }
-                } else {
-                    if (new_nodo.getPunto().getY() < aux_ptr.getPunto().getY()) {
-                        if (aux_ptr.getLeft() != null) {
-                            aux_ptr = aux_ptr.getLeft();
-                        } else {
-                            aux_ptr.setLeft(new_nodo);
-                            return;
-                        }
-                    } else {
-                        if (aux_ptr.getRight() != null) {
-                            aux_ptr = aux_ptr.getRight();
-                        } else {
-                            aux_ptr.setRight(new_nodo);
-                            return;
-                        }
-                    }
-                }
-                inX = !inX;
+            node_push(root, true, new_nodo);
+        }
+    }
+
+    private Nodo node_push(Nodo current_node, boolean inX, Nodo new_node) {
+        if (current_node == null) {
+            return new_node;
+        }
+
+        if (inX) {
+            if (new_node.getPunto().getX() < current_node.getPunto().getX()) {
+                current_node.setLeft(node_push(current_node.getLeft(), !inX, new_node));
+            } else {
+                current_node.setRight(node_push(current_node.getRight(), !inX, new_node));
+            }
+        } else {
+            if (new_node.getPunto().getY() < current_node.getPunto().getY()) {
+                current_node.setLeft(node_push(current_node.getLeft(), !inX, new_node));
+            } else {
+                current_node.setRight(node_push(current_node.getRight(), !inX, new_node));
             }
         }
+
+        return current_node;
     }
 
     @Override
