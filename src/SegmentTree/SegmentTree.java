de09d8faf1c74bac9a76d1d3cfe027567e14336f
Johanes Osmayo <joh.osm@gmail.com>
2026-06-10 19:28:18 -0500

    Arreglado bug al crear el segmentTree

--- src/SegmentTree/SegmentTree.java
+++ src/SegmentTree/SegmentTree.java
@@ -13,7 +13,7 @@ public class SegmentTree {
         if (left == right) {
             return new Nodo<Integer>(arr[left]);
         } else {
-            int middle = (right - left)/2;
+            int middle = (right - left)/2 + left;
             Nodo<Integer> leftNode = build(arr, left, middle);
             Nodo<Integer> rightNode = build(arr, middle + 1, right);
             Integer value = leftNode.getValue() + rightNode.getValue();
@@ -31,5 +31,37 @@ public class SegmentTree {
         } else {
             return head.toString();
         }
+    }
+
+    public Integer query(int left, int right) {
+        if (left > right || right >= len || left < 0) {
+            new RuntimeException("Invalid indexes.");
+        }
+        if (left == 0 && right == len - 1) {
+            return head.getValue();
+        } else {
+            return query(head, left, right, 0, len - 1);
+        }
+    }
+
+    private Integer query(Nodo<Integer> currentNode,int queryLeft, int queryRight, int currentLeft, int currentRight) {
+        int middle = (currentRight - currentLeft)/2;
+        if (queryLeft == currentLeft) {
+            if (currentRight <= queryRight) {
+                return currentNode.getValue();
+            } else {
+                return query(currentNode.getLeft(), queryLeft, queryRight, currentLeft, middle);
+            }
+        }
+        if (queryRight == currentRight) {
+            if (currentLeft >= queryLeft) {
+                return currentNode.getValue();
+            } else {
+                return query(currentNode.getRight(), queryLeft, queryRight, middle + 1, currentRight);
+            }
+        }
+        int left = query(currentNode.getLeft(), queryLeft, queryRight, currentLeft, middle);
+        int right = query(currentNode.getRight(), queryLeft, queryRight, middle + 1, currentRight);
+        return left + right;
     }
 }
--- src/TestSegmentTree.java
+++ src/TestSegmentTree.java
@@ -2,7 +2,7 @@ import SegmentTree.SegmentTree;
 
 public class TestSegmentTree {
     public TestSegmentTree() {
-        int[] arr = {1, 2, 3};
+        int[] arr = {1, 2, 3, 4};
         SegmentTree tree = new SegmentTree(arr);
         System.out.println(tree);
     }
