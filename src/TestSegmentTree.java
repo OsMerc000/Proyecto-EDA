88e8fec4c22795a1db157473681966c8c418b7ae
Johanes Osmayo <joh.osm@gmail.com>
2026-06-15 07:54:13 -0500

    Implementado el segmentTree para datos genéricos

--- src/TestSegmentTree.java
+++ src/TestSegmentTree.java
@@ -1,9 +1,16 @@
 import SegmentTree.SegmentTree;
+import SegmentTree.SegmentTree.Associative;
 
 public class TestSegmentTree {
     public TestSegmentTree() {
-        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7};
-        SegmentTree tree = new SegmentTree(arr);
+        Integer[] arr = {0, 1, 2, 3, 4, 5, 6, 7};
+        Associative<Integer> method = new Associative<Integer>() {
+            @Override
+            public Integer method(Integer u, Integer v) {
+                return u + v;
+            }
+        };
+        SegmentTree<Integer> tree = new SegmentTree<Integer>(arr, method);
         System.out.println("Tree:\n" + tree);
         System.out.println("arr[0] + ... + arr[2] = " + tree.query(0, 2));
         System.out.println("arr[5] + ... + arr[7] = " + tree.query(5, 7));
